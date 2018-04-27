f676c82f301521303cb9a2f99f5be1ab96910c00
“x-raf <x-raf@wp.pl>
2018-04-27 19:54:31 +0200

    first commit

---
+++ src/main/java/com/kodilla/kodillacalculator/KodillaCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.kodillacalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCalculatorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kodilla/kodillacalculator/KodillaCalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.kodillacalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodillaCalculatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
