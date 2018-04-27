3fb4c5bb35308d22886d6596925fc7a6333f06fe
“x-raf <x-raf@wp.pl>
2018-04-27 20:21:33 +0200

    second commit

--- src/main/java/com/kodilla/kodillacalculator/KodillaCalculatorApplication.java
+++ src/main/java/com/kodilla/kodillacalculator/KodillaCalculatorApplication.java
@@ -2,11 +2,18 @@ package com.kodilla.kodillacalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 @SpringBootApplication
 public class KodillaCalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaCalculatorApplication.class, args);
+
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int result = simpleCalculator.addAToB(5, 5);
+        System.out.println("Wynik to: " + result);
     }
 }
---
+++ src/main/java/com/kodilla/kodillacalculator/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.kodillacalculator;
+
+public class SimpleCalculator {
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+}
